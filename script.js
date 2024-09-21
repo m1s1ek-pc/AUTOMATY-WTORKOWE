16744794315175f2a31378c9745ee34bfb7311de
m1s1ek-pc <piotrekm2010@gmail.com>
2024-09-21 10:32:16 +0200

    Update script.js

--- script.js
+++ script.js
@@ -36,101 +36,102 @@ function spin() {
         const finalDisplay = `${slot1} ${slot2} ${slot3}`;
         slotDisplay.textContent = finalDisplay;
 
-        let coinsWon = calculateWin([slot1, slot2, slot3]);
-        updateCoins(coinsWon);
+        let multiplier = calculateWin([slot1, slot2, slot3]);
+        updateCoins(multiplier);
     }, 2000); // Czas animacji: 2 sekundy
 }
 
 function calculateWin(numbers) {
     const [slot1, slot2, slot3] = numbers;
-    let coinsWon = 0;
+    let multiplier = 0;
 
-    // Nagrody za wygrane kombinacje
+    // Nagrody za wygrane kombinacje z mnożnikami
     if (slot1 === 7 && slot2 === 7 && slot3 === 7) {
-        coinsWon = 1000;
-        document.getElementById('result').textContent = "777 - Jackpot! +1000 coinów";
+        multiplier = 1000; // Jackpot
+        document.getElementById('result').textContent = "777 - Jackpot! x1000";
     } else if (slot1 === 9 && slot2 === 1 && slot3 === 9) {
-        coinsWon = 100;
-        document.getElementById('result').textContent = "919 - BIG WIN! +100 coinów";
+        multiplier = 10;
+        document.getElementById('result').textContent = "919 - BIG WIN! x10";
     } else if (slot1 === 1 && slot2 === 1 && slot3 === 1) {
-        coinsWon = 50;
-        document.getElementById('result').textContent = "111 - WIN! +50 coinów";
+        multiplier = 5;
+        document.getElementById('result').textContent = "111 - WIN! x5";
     } else if (slot1 === 0 && slot2 === 0 && slot3 === 1) {
-        coinsWon = 30;
-        document.getElementById('result').textContent = "001 - AGENT WIN! +30 coinów";
+        multiplier = 3;
+        document.getElementById('result').textContent = "001 - AGENT WIN! x3";
     } else if (slot1 === 6 && slot2 === 6 && slot3 === 6) {
-        coinsWon = 70;
-        document.getElementById('result').textContent = "666 - Super WIN! +70 coinów";
+        multiplier = 7;
+        document.getElementById('result').textContent = "666 - Super WIN! x7";
     } else if (slot1 === 5 && slot2 === 5 && slot3 === 5) {
-        coinsWon = 40;
-        document.getElementById('result').textContent = "555 - Great WIN! +40 coinów";
+        multiplier = 4;
+        document.getElementById('result').textContent = "555 - Great WIN! x4";
     } else if (slot1 === 3 && slot2 === 3 && slot3 === 3) {
-        coinsWon = 25;
-        document.getElementById('result').textContent = "333 - Nice WIN! +25 coinów";
+        multiplier = 2;
+        document.getElementById('result').textContent = "333 - Nice WIN! x2";
     } 
     // Nowe kombinacje wygranych
     else if (slot1 === 4 && slot2 === 8 && slot3 === 4) {
-        coinsWon = 200;
-        document.getElementById('result').textContent = "484 - Lucky Strike! +200 coinów";
+        multiplier = 20;
+        document.getElementById('result').textContent = "484 - Lucky Strike! x20";
     } else if (slot1 === 2 && slot2 === 2 && slot3 === 2) {
-        coinsWon = 60;
-        document.getElementById('result').textContent = "222 - Double Lucky! +60 coinów";
+        multiplier = 6;
+        document.getElementById('result').textContent = "222 - Double Lucky! x6";
     } else if (slot1 === 4 && slot2 === 4 && slot3 === 4) {
-        coinsWon = 50;
-        document.getElementById('result').textContent = "444 - Quadra WIN! +50 coinów";
+        multiplier = 5;
+        document.getElementById('result').textContent = "444 - Quadra WIN! x5";
     } else if (slot1 === 8 && slot2 === 8 && slot3 === 8) {
-        coinsWon = 80;
-        document.getElementById('result').textContent = "888 - Triple Eights! +80 coinów";
+        multiplier = 8;
+        document.getElementById('result').textContent = "888 - Triple Eights! x8";
     } else if (slot1 === 2 && slot2 === 5 && slot3 === 2) {
-        coinsWon = 90;
-        document.getElementById('result').textContent = "252 - Power Combo! +90 coinów";
+        multiplier = 9;
+        document.getElementById('result').textContent = "252 - Power Combo! x9";
     } else if (slot1 === 7 && slot2 === 2 && slot3 === 7) {
-        coinsWon = 100;
-        document.getElementById('result').textContent = "727 - Lucky Wings! +100 coinów";
+        multiplier = 10;
+        document.getElementById('result').textContent = "727 - Lucky Wings! x10";
     } else if (slot1 === 3 && slot2 === 6 && slot3 === 9) {
-        coinsWon = 70;
-        document.getElementById('result').textContent = "369 - Triple Energy! +70 coinów";
+        multiplier = 7;
+        document.getElementById('result').textContent = "369 - Triple Energy! x7";
     } else if (slot1 === 8 && slot2 === 3 && slot3 === 8) {
-        coinsWon = 75;
-        document.getElementById('result').textContent = "838 - Power Win! +75 coinów";
+        multiplier = 7;
+        document.getElementById('result').textContent = "838 - Power Win! x7";
     } else if (slot1 === 4 && slot2 === 1 && slot3 === 4) {
-        coinsWon = 90;
-        document.getElementById('result').textContent = "414 - Double Delight! +90 coinów";
+        multiplier = 9;
+        document.getElementById('result').textContent = "414 - Double Delight! x9";
     } else if (slot1 === 7 && slot2 === 5 && slot3 === 7) {
-        coinsWon = 110;
-        document.getElementById('result').textContent = "757 - Triple Jackpot! +110 coinów";
+        multiplier = 12;
+        document.getElementById('result').textContent = "757 - Triple Jackpot! x12";
     } else if (slot1 === 5 && slot2 === 2 && slot3 === 5) {
-        coinsWon = 120;
-        document.getElementById('result').textContent = "525 - Special Win! +120 coinów";
+        multiplier = 13;
+        document.getElementById('result').textContent = "525 - Special Win! x13";
     } else if (slot1 === 4 && slot2 === 2 && slot3 === 4) {
-        coinsWon = 130;
-        document.getElementById('result').textContent = "424 - Major Prize! +130 coinów";
+        multiplier = 14;
+        document.getElementById('result').textContent = "424 - Major Prize! x14";
     } else if (slot1 === 8 && slot2 === 1 && slot3 === 8) {
-        coinsWon = 150;
-        document.getElementById('result').textContent = "818 - Mega Lucky! +150 coinów";
+        multiplier = 15;
+        document.getElementById('result').textContent = "818 - Mega Lucky! x15";
     } else if (slot1 === 2 && slot2 === 4 && slot3 === 2) {
-        coinsWon = 160;
-        document.getElementById('result').textContent = "242 - Lucky Double! +160 coinów";
+        multiplier = 16;
+        document.getElementById('result').textContent = "242 - Lucky Double! x16";
     } else if (slot1 === 9 && slot2 === 2 && slot3 === 9) {
-        coinsWon = 180;
-        document.getElementById('result').textContent = "929 - Ultra WIN! +180 coinów";
+        multiplier = 18;
+        document.getElementById('result').textContent = "929 - Ultra WIN! x18";
     } else if (slot1 === 3 && slot2 === 9 && slot3 === 3) {
-        coinsWon = 200;
-        document.getElementById('result').textContent = "393 - Jackpot Trio! +200 coinów";
+        multiplier = 20;
+        document.getElementById('result').textContent = "393 - Jackpot Trio! x20";
     } else if (slot1 === 9 && slot2 === 3 && slot3 === 9) {
-        coinsWon = 220;
-        document.getElementById('result').textContent = "939 - King’s WIN! +220 coinów";
+        multiplier = 22;
+        document.getElementById('result').textContent = "939 - King’s WIN! x22";
     } else if (slot1 === 4 && slot2 === 5 && slot3 === 4) {
-        coinsWon = 240;
-        document.getElementById('result').textContent = "454 - Super Bonus! +240 coinów";
+        multiplier = 24;
+        document.getElementById('result').textContent = "454 - Super Bonus! x24";
     } else {
         document.getElementById('result').textContent = "Spróbuj ponownie!";
     }
 
-    return coinsWon;
+    return multiplier;
 }
 
-function updateCoins(coinsWon) {
+function updateCoins(multiplier) {
+    const coinsWon = stake * multiplier;
     coins += coinsWon;
     document.getElementById('coins').textContent = `Coiny: ${coins}`;
     stake = 0; // Resetuj stawkę
@@ -152,4 +153,3 @@ function applyCode() {
         codeResult.textContent = "Nieprawidłowy kod!";
     }
 }
-
